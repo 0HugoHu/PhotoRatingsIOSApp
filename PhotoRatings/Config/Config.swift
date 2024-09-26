519d975c4a6e42202f90a65be0003f0531628c90
0HugoHu <0@hugohu.top>
2024-09-25 19:43:53 -0700

    Fix: clean work folder tree

---
+++ PhotoRatings/Config/Config.swift
@@ -0,0 +1,14 @@
+//
+//  Config.swift
+//  PhotoRatings
+//
+//  Created by Hugooooo on 9/22/24.
+//
+
+
+public let LOGIN: String = "/photo_ratings_login"
+public let GET_IMAGE_LIST: String = "/get_unrated_images"
+public let GET_IMAGE: String = "/images"
+public let RATE_IMAGE: String = "/rate_image"
+
+public let IMAGE_COUNT = 10
